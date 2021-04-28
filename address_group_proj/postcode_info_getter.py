96d230162a0a31c109bdd36296a66ffb87970f43
AlexlwAstro <alexander.lw@me.com>
2021-04-28 17:40:08 +0100

    started looking at unit tests, ways to extract individual bit of info per postcode

---
+++ address_group_proj/postcode_info_getter.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+from pprint import pprint
+"""
+post_code_req = requests.post('https://api.postcodes.io/postcodes/mk139hd')
+print(type(post_code_req.headers))
+print(type(post_code_req.content))  # class 'bytes'
+print(post_code_req.json())
+"""
+
+json_body = json.dumps({"postcodes": ["OX49 5NU", "M32 0JG", "NE30 1DP", "L8 7NW"]})
+headers = {'Content-Type': 'application/json'}
+
+try:
+    post_multi_req = requests.post("https://api.postcodes.io/postcodes", headers=headers, data=json_body)
+except ConnectionError:
+    print('URL not recognised! Try again')
+
+result_json = post_multi_req.json()
+results = result_json['result']
+print(len(results))
+for item in results:
+    pprint(item)
