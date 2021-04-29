442af8aefeb0a03d6b6b7db3f84dc51497df6bbf
jagman014 <72930861+jagman014@users.noreply.github.com>
2021-04-29 14:13:51 +0100

    update with config files

--- address_group_proj/getpostcodes/postcode_variables.py
+++ address_group_proj/getpostcodes/postcode_variables.py
@@ -1,28 +1,11 @@
-available_info_categories = {
-                            0: "postcode", 
-                            1: "quality", 
-                            2: "eastings", 
-                            3: "northings", 
-                            4: "country", 
-                            5: "nhs_ha", 
-                            6: "longitude",
-                            7: "latitude", 
-                            8: "european_electoral_region", 
-                            9: "primary_care_trust", 
-                            10: "region", 
-                            11: "lsoa", 
-                            12: "msoa",
-                            13: "incode", 
-                            14: "outcode", 
-                            15: "parliamentary_constituency", 
-                            16: "admin_district", 
-                            17: "parish",
-                            18: "admin_county", 
-                            19: "admin_ward", 
-                            20: "ced", 
-                            21: "ccg",
-                            22: "nuts", 
-                            23: "codes"
-                        }
+from configparser import ConfigParser
 
-inds_list = [0, 4, 15]
+config = ConfigParser()
+config.read('getpostcodes/config.ini')
+
+inds_list = config['CHOSEN CATEGORIES']['INDEX'].split(',')
+
+info_categories = [config['CATEGORIES'][index.strip()] for index in inds_list]
+
+postcodes = [config['POSTCODES'][value.upper()] 
+            for value in list(config['POSTCODES'])]
